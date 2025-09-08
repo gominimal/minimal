a71df7187adc6117850ec6833bac7ae2b8779f27
Brandon Weeks <brandon@minimal.dev>
2025-09-08 21:13:32 +0000

    introduce base metapackage, simplify packages

--- test_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "=== Environment Test ==="
-echo "Number of environment variables: $(env | wc -l)"
-echo
-echo "Environment variables:"
-env | sort
-echo
-echo "=== Test complete ==="
-exit
