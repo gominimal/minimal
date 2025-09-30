a8b5864ef977b66ad3b56c006afaba1efa421dd8
Brandon Weeks <me@brandonweeks.com>
2025-09-30 15:42:43 -0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  nixConfig.bash-prompt = "[nix(minpkgs)] ";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          cargo
+          clippy
+          rust-analyzer
+          rustc
+          rustfmt
+          openssl
+        ];
+      };
+    };
+}
