445ff98c5f94a6db16b9d14bfb49ebdd79cd07d5
Tom <tom@minimal.dev>
2025-10-16 14:02:37 -0700

    packages: delete, its now in gominimal/pkgs

--- packages/btop/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-cd btop-1.4.5
-
-# Disable GPU paths at compile time
-make -j"$(nproc)" GPU_SUPPORT=false
-make DESTDIR="${OUTPUT_DIR}" PREFIX="/usr" install
--- packages/setuptools/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-tar xf setuptools-80.9.0.tar.gz
-cd setuptools-80.9.0
-
-pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
-pip3 install --root $OUTPUT_DIR --no-index --find-links dist setuptools
