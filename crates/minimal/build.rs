991c75c69362b954d4afa5e414bf0f1114bb90fe
Brandon Weeks <bweeks@framework.exfil.it>
2025-12-04 11:05:21 -0800

    Add 30 day kill switch

--- crates/minimal/build.rs
+++ crates/minimal/build.rs
@@ -6,4 +6,10 @@ fn main() {
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    println!("cargo:rustc-env=BUILD_TIMESTAMP={}", timestamp);
 }
