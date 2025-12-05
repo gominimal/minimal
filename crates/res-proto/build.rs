5e27a3d4374be4bed3d04a0e7b47146effff7178
Tom <tom@minimal.dev>
2025-12-05 15:12:43 -0800

    crates/res-proto: implement service and wire protos for RES

--- crates/res-proto/build.rs
+++ crates/res-proto/build.rs
@@ -1,8 +1,21 @@
 use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &["protos/injected_files.proto", "protos/tarball_format.proto"],
+        &[
+            "protos/injected_files.proto",
+            "protos/tarball_format.proto",
+            "protos/command_parameters.proto",
+            "protos/create_build.proto",
+            "protos/poll_build.proto",
+            "protos/upload.proto",
+            "protos/remote_execution_service.proto",
+        ],
         &["protos/"],
     )?;
+
+    tonic_prost_build::configure()
+        .compile_protos(&["protos/remote_execution_service.proto"], &["protos"])
+        .unwrap();
+
     Ok(())
 }
