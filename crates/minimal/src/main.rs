ef7881357c64dccf6ec445624fd65f6aae60da80
ms <ms@ms-debian>
2026-01-13 20:37:47 -0800

    removed original graphviz 'dependencies' subcommand

--- crates/minimal/src/main.rs
+++ crates/minimal/src/main.rs
@@ -34,8 +34,6 @@ use cmd_patched_build::{PatchedBuildArgs, cmd_patched_build};
 mod cmd_run;
 #[cfg(target_os = "linux")]
 use cmd_run::{RunArgs, cmd_run};
-mod cmd_dependencies;
-use cmd_dependencies::{DependenciesArgs, cmd_dependencies};
 mod cmd_dep;
 use cmd_dep::{DepArgs, cmd_dep};
 mod cmd_update;
@@ -74,12 +72,6 @@ enum Command {
     /// Executes the build for a package, using stale dependencies.
     #[clap(hide = !std::env::var("MINIMAL_SCIENCE_MODE").is_ok())]
     PatchedBuild(PatchedBuildArgs),
-    /// Generates Graphviz source code of the dependency graph
-    #[clap(hide = !std::env::var("MINIMAL_SCIENCE_MODE").is_ok())]
-    #[command(
-        long_about = "Generate an image of the dependency graph using graphviz's \"dot\" program.\n\n  minimal dependencies -p gcc | dot -Tpng > deps.png\n"
-    )]
-    Dependencies(DependenciesArgs),
     /// Generates Graphviz source code of the dependency graph
     #[clap(hide = !std::env::var("MINIMAL_SCIENCE_MODE").is_ok())]
     #[command(
@@ -619,10 +611,8 @@ async fn run_cli(cli: Cli) -> Result<(), Error> {
         Command::PatchedBuild(args) => cmd_patched_build(args, &mut ctx).await,
         #[cfg(target_os = "linux")]
         Command::Run(args) => cmd_run(args, &mut ctx).await,
-        Command::Dependencies(args) => cmd_dependencies(args, &mut ctx).await,
-        Command::GenerateCompletions(_) => Ok(()),
-
         Command::Update(args) => cmd_update(args, &mut ctx).await,
         Command::Dep(args) => cmd_dep(args, &mut ctx).await,
+        Command::GenerateCompletions(_) => Ok(()),
     }
 }
