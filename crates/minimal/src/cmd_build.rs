feb4e5469fc1f35964a0689a6dde149f0fc18b0c
Tom <tom@minimal.dev>
2026-01-29 20:35:16 -0800

    crates/minimal: fix minimal build doing nothing without -p

--- crates/minimal/src/cmd_build.rs
+++ crates/minimal/src/cmd_build.rs
@@ -11,7 +11,29 @@ pub struct BuildArgs {
 
 pub async fn cmd_build(args: BuildArgs, ctx: &mut Context) -> Result<(), Error> {
     trace!("cmd_build");
-    let graph = ctx.graph_from_package_names(args.packages.names())?;
+    let graph = match args
+        .packages
+        .packages
+        .as_ref()
+        .map(|v| v.len())
+        .unwrap_or(0)
+    {
+        // Nothing was specified - build everything in the graph defined in the top-level repo.
+        0 => {
+            let mut graph = ctx.graph_from_all_packages()?;
+
+            // TODO: Don't just assume that stuff that came from a directory instead of a
+            // github repo is the top level.
+            graph.top_levels = graph
+                .top_levels
+                .iter()
+                .filter(|bsr| graph.get(&bsr).unwrap().from.as_repo().is_none())
+                .copied()
+                .collect();
+            graph
+        }
+        _ => ctx.graph_from_package_names(args.packages.names())?,
+    };
     let cache = ctx.local_cache();
 
     cmd_build_impl(&graph, ctx, cache, false).await?;
