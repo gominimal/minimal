1236d27043123abfa79a9a6f1a58ac21c1b3ea8f
Mike <mike.sample@gmail.com>
2026-02-03 09:52:54 -0800

    crates/common: add #cfg block to compute minimal CLI target arch from the host arch that works on Linux aarch64

--- crates/common/src/target.rs
+++ crates/common/src/target.rs
@@ -39,6 +39,14 @@ impl Target {
             }
         }
 
+        #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+        {
+            Self {
+                arch: Arch::Arm64,
+                os: OS::Linux,
+            }
+        }
+
         #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
         {
             Self {
