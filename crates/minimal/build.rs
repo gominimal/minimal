5359dccc41943caa1008124d021e7d33856bafab
Tom <tom@minimal.dev>
2026-04-15 10:57:14 -0700

    crates/minimal: remove build expiry lameduck logic

--- crates/minimal/build.rs
+++ crates/minimal/build.rs
@@ -7,12 +7,6 @@ fn main() {
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 
-    let timestamp = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    println!("cargo:rustc-env=BUILD_TIMESTAMP={}", timestamp);
-
     let is_clean = std::process::Command::new("git")
         .args(["status", "--porcelain=v1"])
         .output()
