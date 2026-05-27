1c7cf3ff416ae0b67bc5531a44a71b5275a2672a
Tom <tom@minimal.dev>
2026-05-26 22:09:27 -0700

    crates/minimald: create scaffolding for building up SSH channels

---
+++ crates/minimald/src/lib.rs
@@ -0,0 +1,23 @@
+use std::collections::BTreeMap;
+
+pub mod connection;
+pub mod rpc;
+pub mod server;
+
+/// Represents the parameters of a requested PTY.
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub struct RequestedPty {
+    char_sizes: (u32, u32),
+    pixel_sizes: (u32, u32),
+    term: String,
+    modes: Vec<(russh::Pty, u32)>,
+}
+
+/// Represents the currently configured parameters for a channel being created.
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ChannelConfig {
+    pub(crate) env_vars: BTreeMap<String, String>,
+    pub(crate) pty: Option<RequestedPty>,
+}
--- crates/minimald/src/server.rs
+++ crates/minimald/src/server.rs
@@ -4,11 +4,12 @@ use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::net::UnixListener;
+use tokio::sync::Mutex;
 use tokio::task::JoinSet;
 
 use crate::connection::Connection;
 
-/// The host private key for the SSH server.
+/// The ed25519 host private key for the SSH server.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum HostKey {
@@ -16,27 +17,90 @@ pub enum HostKey {
     Ephemeral,
     /// An OpenSSH-formatted PEM private key.
     Raw(String),
-    /// A path to an OpenSSH-formatted PEM private key.
-    Path(PathBuf),
+    /// A path where an OpenSSH-formatted PEM private key should be stored,
+    /// optionally created on first use.
+    OnDisk {
+        path: PathBuf,
+        create_if_missing: bool,
+    },
 }
 
 /// Global Configuration for the minimald server.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Config {
     pub host_key: HostKey,
-    pub minimal_dir: PathBuf,
+    pub minimal_state_dir: PathBuf,
+    pub minimal_cache_dir: PathBuf,
 }
 
 impl Config {
     /// Returns the SSH host key to use.
+    ///
+    /// For [`HostKey`] variant `OnDisk{ create_on_missing: true, ..}`,
+    /// a new key will be generated and written if the file does not exist.
     pub fn host_key(&self) -> Result<PrivateKey, KeyError> {
         match &self.host_key {
             HostKey::Ephemeral => {
                 let key = PrivateKey::random(&mut safe_rng(), russh::keys::Algorithm::Ed25519)?;
                 Ok(key)
             }
-            HostKey::Path(path) => Ok(PrivateKey::read_openssh_file(path)?),
+            HostKey::OnDisk {
+                path,
+                create_if_missing,
+            } => match PrivateKey::read_openssh_file(path) {
+                Ok(k) => Ok(k),
+                Err(KeyError::Io(std::io::ErrorKind::NotFound)) => {
+                    if *create_if_missing {
+                        let key =
+                            PrivateKey::random(&mut safe_rng(), russh::keys::Algorithm::Ed25519)?;
+                        key.write_openssh_file(path, russh::keys::ssh_key::LineEnding::LF)?;
+                        Ok(key)
+                    } else {
+                        Err(KeyError::Io(std::io::ErrorKind::NotFound))
+                    }
+                }
+                Err(e) => Err(e),
+            },
             HostKey::Raw(r) => Ok(PrivateKey::from_openssh(r.as_bytes())?),
+        }
+    }
+}
+
+/// A container for the state of the server.
+#[derive(Debug)]
+pub struct ServerState {
+    config: Config,
+
+    /// Memoized SSH host key, after first successful load.
+    host_key: Option<PrivateKey>,
+}
+
+impl ServerState {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            host_key: None,
+        }
+    }
+}
+
+/// A thread-safe handle to the server state.
+#[derive(Clone, Debug)]
+pub struct ServerStateHandle(Arc<Mutex<ServerState>>);
+
+impl ServerStateHandle {
+    pub async fn host_key(&self) -> Result<PrivateKey, KeyError> {
+        let mut s = self.0.lock().await;
+        if let Some(hk) = &s.host_key {
+            return Ok(hk.clone());
+        }
+
+        match s.config.host_key() {
+            Ok(hk) => {
+                s.host_key = Some(hk.clone());
+                Ok(hk)
+            }
+            Err(e) => Err(e),
         }
     }
 }
@@ -44,20 +108,22 @@ impl Config {
 /// A listening minimald server.
 #[derive(Debug)]
 pub struct Server {
-    config: Config,
+    state: ServerStateHandle,
     listener: UnixListener,
 }
 
 impl Server {
     /// Launches minimald, listening for connections on the given UDS socket.
     pub async fn run_on_uds(config: Config, listener: UnixListener) -> Result<(), std::io::Error> {
-        Server { config, listener }.run().await
+        let state = ServerStateHandle(Arc::new(Mutex::new(ServerState::new(config))));
+        Server { state, listener }.run().await
     }
 
     async fn run(self) -> Result<(), std::io::Error> {
         let russh_config = Arc::new(russh::server::Config {
-            keys: vec![self.config.host_key().unwrap()],
+            keys: vec![self.state.host_key().await.unwrap()],
             auth_rejection_time_initial: Some(std::time::Duration::ZERO),
+            nodelay: true,
             ..Default::default()
         });
 
@@ -65,7 +131,8 @@ impl Server {
         loop {
             let (socket, _) = self.listener.accept().await?;
             let (_conn_hnd, session_fut) =
-                Connection::from_socket(socket, russh_config.clone(), true).await;
+                Connection::from_socket(socket, russh_config.clone(), self.state.clone(), true)
+                    .await;
             session_set.spawn(session_fut);
         }
     }
